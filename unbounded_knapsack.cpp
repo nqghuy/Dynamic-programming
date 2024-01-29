5f14eefa3c7cd291bb2151dae2cfc218c5f8accb
nqghuy <kehi4705@gmail.com>
2024-01-29 22:10:29 +0700

    another solution

--- unbounded_knapsack.cpp
+++ unbounded_knapsack.cpp
@@ -32,32 +32,43 @@ int solve(){
 
         //contains number with maximum number of repetitions
         vector <int> v;
-
+        v.push_back(0);
         for (int i : se){
             v.push_back(i);
         }
         
         //dp[i][j] is the maximum sum with limited sum j
-        int dp[v.size() + 1][k + 1];
-        memset(dp, 0, sizeof(dp));
+        // int dp[v.size() + 1][k + 1];
+        // memset(dp, 0, sizeof(dp));
 
-        for (int i = 1; i <= v.size(); i++){
-            for (int j = 1; j <= k; j++){
-                dp[i][j] = dp[i - 1][j];
+        // for (int i = 1; i <= v.size(); i++){
+        //     for (int j = 1; j <= k; j++){
+        //         dp[i][j] = dp[i - 1][j];
                 
-                //if can add up current value
-                if (j >= v[i - 1]){
-                    dp[i][j] = max(dp[i][j], dp[i][j - v[i - 1]] + v[i - 1]);
-                }
-            }
-        }
+        //         //if can add up current value
+        //         if (j >= v[i - 1]){
+        //             dp[i][j] = max(dp[i][j], dp[i][j - v[i - 1]] + v[i - 1]);
+        //         }
+        //     }
+        // }
         // for (int i = 0; i <= v.size(); i++){
         //     for (int j = 0; j <= k; j++){
         //         cout << dp[i][j] << " ";
         //     }
         //     cout << endl;
         // }
-        return dp[v.size()][k];
+        int dp[k + 1];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= v.size(); i++){
+            for (int j = 0; j <= k; j++){
+                if (j >= v[i]){
+                    dp[j] = max(dp[j], dp[j - v[i]] + v[i]);
+                }
+                if (dp[j] == k) return k;
+            }
+        }
+        // for (int i : dp)    cout << i << " ";
+        return dp[k];
 }
 
 int main(){
